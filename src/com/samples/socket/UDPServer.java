ebab1a0e8a7a41261d5c7583592120132f073096
dhlee <dhlee@eactive.co.kr>
2024-03-22 13:44:20 +0900

    update code

--- src/com/samples/socket/UDPServer.java
+++ src/com/samples/socket/UDPServer.java
@@ -1,9 +1,36 @@
 package com.samples.socket;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPServer {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
 
-	public UDPServer() {
-		// TODO Auto-generated constructor stub
-	}
+        try {
+            socket = new DatagramSocket(9876);
 
+            byte[] receiveData = new byte[1024];
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + message);
+
+                String responseMessage = "Hello, client!";
+                byte[] sendData = responseMessage.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                socket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }
+    }
 }
+
